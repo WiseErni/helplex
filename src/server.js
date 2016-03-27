72a33e6d0e11bf884e1fce32f86aa2cfa1ee8388
tinkerscript <river.burbot@yandex.ru>
2016-03-27 23:44:45 +0500

    logging prototype

--- src/server.js
+++ src/server.js
@@ -2,15 +2,35 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const expressWinston = require('express-winston');
+const winston = require('winston');
 const routes = require('./routes');
 const app = express();
+const SeqLogger = require('./seqlogger');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: false
 }));
 
+app.use(expressWinston.logger({
+  transports: [
+    new winston.transports.File({
+      json: false,
+      filename: './logs/express.log'
+    })
+  ]
+}));
+
 app.use('/', routes);
+
+app.use(expressWinston.errorLogger({
+  transports: [
+    new SeqLogger({
+      json: false
+    })
+  ]
+}));
 
 // eslint-disable-next-line
 app.use((err, req, res, next) => {
